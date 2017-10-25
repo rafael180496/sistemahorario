5271c880638f65646cf3be216518ce822a5df158
Rafael Hidalgo <rhidalgo@tda.thedataage.com>
2017-10-25 15:05:12 -0600

    RAFAEL

--- Base de datos/sistema_de_horario.sql
+++ Base de datos/sistema_de_horario.sql
@@ -1,2 +1,18 @@
     CREATE DATABASE shorario;
     
+    CREATE TABLE aula (
+        id_aula SERIAL PRIMARY KEY,
+        desc_aula TEXT,
+        ind_mant BOOLEAN,
+        f_creacion DATE
+    );
+
+
+    CREATE TABLE profesor (
+        id_profesor SERIAL PRIMARY KEY,
+        nombre  TEXT, 
+        apellido TEXT,
+        f_creacion DATE 
+    );
+
+
