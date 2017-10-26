b8981a453c685fa301cbde0ba3d0f55698d4297a
Rafael Hidalgo <rhidalgo@tda.thedataage.com>
2017-10-26 13:43:25 -0600

    RAFAEL

--- Base de datos/sistema_de_horario.sql
+++ Base de datos/sistema_de_horario.sql
@@ -1,18 +1,72 @@
     CREATE DATABASE shorario;
     
     CREATE TABLE aula (
-        id_aula SERIAL PRIMARY KEY,
-        desc_aula TEXT,
-        ind_mant BOOLEAN,
-        f_creacion DATE
+        id_aula SERIAL PRIMARY KEY NOT NULL,
+        desc_aula TEXT NOT NULL,
+        ind_mant BOOLEAN NOT NULL,
+        f_creacion DATE NOT NULL
     );
 
 
     CREATE TABLE profesor (
-        id_profesor SERIAL PRIMARY KEY,
-        nombre  TEXT, 
-        apellido TEXT,
-        f_creacion DATE 
+        id_profesor SERIAL PRIMARY KEY NOT NULL,
+        nombre  TEXT NOT NULL, 
+        apellido TEXT NOT NULL, 
+        f_creacion DATE NOT NULL
+    );
+
+    CREATE TABLE carrera (
+        id_carrera SERIAL PRIMARY KEY NOT NULL,
+        nombre  TEXT NOT NULL, 
+        f_creacion DATE  NOT NULL
+    );
+
+    CREATE TABLE usuario (
+        id_carrera SERIAL PRIMARY KEY NOT NULL,
+        nom_usr  CHAR(50) NOT NULL, 
+        clave CHAR(30) NOT NULL,
+        id_profesor INT NOT NULL,
+        ind_adm BOOLEAN NOT NULL,
+        f_creacion DATE NOT NULL
+    );
+
+     CREATE TABLE clase  (
+        id_clase SERIAL PRIMARY KEY NOT NULL,
+        nombre  TEXT NOT NULL, 
+        f_creacion DATE  NOT NULL
+    );
+
+    CREATE TABLE alumno (
+        carnet CHAR(15) PRIMARY KEY NOT NULL,
+        nombre  TEXT NOT NULL, 
+        apellido TEXT NOT NULL,
+        id_carrera INT NOT NULL,
+        f_creacion    DATE  NOT NULL
+    );
+    CREATE TABLE grupo (
+        id_grupo INT  NOT NULL,
+        id_profesor  INT NOT NULL, 
+        id_clase INT NOT NULL,
+        nombre TEXT NOT NULL,
+        f_creacion    DATE  NOT NULL
+    );
+    CREATE TABLE det_grupo (
+        carnet CHAR(15)  NOT NULL,
+        id_grupo  INT NOT NULL, 
+        f_creacion    DATE  NOT NULL
+    );
+
+     CREATE TABLE horario (
+        id_horario SERIAL PRIMARY KEY NOT NULL,
+        id_clase INT  NOT NULL,
+        id_grupo INT  NOT NULL,
+        id_aula INT  NOT NULL,
+        dia  TEXT NOT NULL, 
+        hora_inicio    INT  NOT NULL,
+        min_inicio INT  NOT NULL,
+        hora_fin INT  NOT NULL,
+        min_fin INT  NOT NULL,
+        f_creacion    DATE  NOT NULL
     );
 
 
