909c4374eee451a821408d12bf154ac8dcc99fae
Rafael Hidalgo <rhidalgo@tda.thedataage.com>
2017-10-27 11:35:33 -0600

    rafael

--- Sistema/horario/src/main/java/com/horario/Service/Interface/IUsuario.java
+++
@@ -1,8 +0,0 @@
-package com.horario.Service.Interface;
-
-
-import com.horario.Model.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IUsuario extends CrudRepository<Usuario, Long> {
-}
