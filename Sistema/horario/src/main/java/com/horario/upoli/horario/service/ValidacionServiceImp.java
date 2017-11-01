d68eeaaf0a76284a043ad4b6da6cc974e1535bfc
Rafael Hidalgo <rhidalgo@tda.thedataage.com>
2017-11-01 17:29:44 -0600

    RAfael

---
+++ Sistema/horario/src/main/java/com/horario/upoli/horario/service/ValidacionServiceImp.java
@@ -0,0 +1,9 @@
+package com.horario.upoli.horario.service;
+
+import com.horario.upoli.horario.repo.ProfesorRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ValidacionServiceImp {
+    @Autowired
+    ProfesorRepo profesorRepo;
+}
