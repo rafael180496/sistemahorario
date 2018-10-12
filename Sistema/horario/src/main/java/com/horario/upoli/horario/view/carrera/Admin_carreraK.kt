57980d426f90586be9d37f6f4bda77c19d7e5603
Rafael Hidalgo <rafael180496@gmail.com>
2018-10-12 09:05:36 -0600

    CAMBIOS DE KOTLIN

--- Sistema/horario/src/main/java/com/horario/upoli/horario/view/carrera/Admin_carreraK.kt
+++ Sistema/horario/src/main/java/com/horario/upoli/horario/view/carrera/Admin_carreraK.kt
@@ -1,2 +1,10 @@
 package com.horario.upoli.horario.view.carrera
 
+import com.horario.upoli.horario.view.componentes.EditorK
+
+class Admin_carreraK:EditorK(){
+    override fun Enviar_Formulario(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
--- Sistema/horario/src/main/java/com/horario/upoli/horario/view/componentes/ExtendsTables.kt
+++ Sistema/horario/src/main/java/com/horario/upoli/horario/view/componentes/ExtendsTables.kt
@@ -1,2 +1,34 @@
 package com.horario.upoli.horario.view.componentes
 
+import com.horario.upoli.horario.model.CarreraK
+import com.horario.upoli.horario.recursos.AccionK
+
+fun ArrayList<CarreraK>.Generar_table(accion:AccionK):String{
+    var Resultado=""
+    Resultado+="""
+        <table class="highlight responsive-table bordered centered">
+            <thead>
+                <tr>
+                    <th>Cod.Reg</th>
+                    <th>Nombre</th>
+                    <th>Fecha</th>
+                    <th>
+                        <a class="btn-floating waves-effect waves-light" href="Add">
+                            <i class="material-icons\">${accion.Add}</i>
+                        </a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+    """
+    for (n in this){
+        Resultado+="""
+           <tr>
+                <td>${n.id_carrera}</td>
+                <td>${n.nombre}</td>
+                <td>${n.f_creacion}</td>
+        """
+    }
+
+    return  Resultado
+}
